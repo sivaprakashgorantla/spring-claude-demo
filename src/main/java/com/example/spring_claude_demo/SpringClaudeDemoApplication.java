a76a954852a33117baf465dedbac2dcf9b3f0a42
sivaprakashgorantla <sivaprakashgorantla@gmail.com>
2025-04-12 21:34:05 +0530

    First commit

---
+++ src/main/java/com/example/spring_claude_demo/SpringClaudeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_claude_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClaudeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClaudeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_claude_demo/SpringClaudeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_claude_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClaudeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
