9347158caeebc171ee64354987d4d2eb93b839df
sivaprakashgorantla <sivaprakashgorantla@gmail.com>
2025-04-14 23:14:10 +0530

    emploee controller test classes

--- src/main/java/com/example/spring_claude_demo/model/Employee.java
+++ src/main/java/com/example/spring_claude_demo/model/Employee.java
@@ -5,9 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 import java.time.LocalDate;
 
